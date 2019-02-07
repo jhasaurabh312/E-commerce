e66b4adcaea741d0f96bdceb91f5b80577670975
jhasaurabh312 <https://github.com/jhasaurabh312>
2019-02-07 10:35:18 +0530

    product array made in .ts page

--- src/app/products-category-body/products-category-body.component.ts
+++ src/app/products-category-body/products-category-body.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { staticProductDetail} from './products-category-body.model';
 
 @Component({
   selector: 'app-products-category-body',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-category-body.component.scss']
 })
 export class ProductsCategoryBodyComponent implements OnInit {
-
+  productDetail: Array<{
+    img: string,
+    _id: string,
+    category:string,
+    product: string,
+    list: string,
+    start_price: number,
+    quantity:number
+    }> = staticProductDetail;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.productDetail.length);
   }
-
+  imageStyle(img) {
+    return {
+      'height': '200px',
+      'width': '200px',
+      'background-image':  `url('../../../assets/images/our-team/${img}')`
+    };
+  }
 }
---
+++ src/app/products-category-body/products-category-body.model.ts
@@ -0,0 +1,12 @@
+export const staticProductDetail = [
+    {
+      img: '' ,
+      _id: '',
+      category: '',
+      product: '',
+      list: '',
+      start_price: 20,
+      quantity: 10
+    },   
+  ];
+  
